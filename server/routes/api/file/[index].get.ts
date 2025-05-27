78c76e2608a529b87dfc9c12881bab2bf55e29c6
satriabha <theblacksteel99@gmail.com>
2025-05-27 23:18:59 +0700

    add assets

--- server/routes/api/file/[index].get.ts
+++ server/routes/api/file/[index].get.ts
@@ -2,39 +2,45 @@ import * as dotenv from "dotenv";
 import { Client, Databases, Storage, Query } from "node-appwrite";
 
 export default defineEventHandler(async (event) => {
-    const client = new Client();
-    const env = dotenv.config()['parsed']
-    console.log(env);
+    // const client = new Client();
+    // const env = dotenv.config()['parsed']
+    // console.log(env);
     
 
-    client
-        .setEndpoint(process.env.APPWRITE_URL)
-        .setProject(process.env.APPWRITE_PROJECT_ID)
-        .setKey(process.env.APPWRITE_API_KEY) // Your secret API key
-    ;
+    // client
+    //     .setEndpoint(env.APPWRITE_URL)
+    //     .setProject(env.APPWRITE_PROJECT_ID)
+    //     .setKey(env.APPWRITE_API_KEY) // Your secret API key
+    // ;
 
-    const storage = new Storage(client)
-    const db = new Databases(client)
+    // const storage = new Storage(client)
+    // const db = new Databases(client)
 
-    const name = getRouterParam(event, 'index')
+    // const docsList = await db.listDocuments(
+    //     env.APPWRITE_DB_ID,
+    //     env.APPWRITE_COLLECTION_FILES_ID,
+    //     [
+    //         Query.equal('filename', [`${name}.csv`])
+    //     ]
+    // ).catch(e => e)
 
-
-    const docsList = await db.listDocuments(
-        process.env.APPWRITE_DB_ID,
-        process.env.APPWRITE_COLLECTION_FILES_ID,
-        [
-            Query.equal('filename', [`${name}.csv`])
-        ]
-    ).catch(e => e)
-
-    const fileID = docsList.documents[0]['fileID'] 
+    // const fileID = docsList.documents[0]['fileID'] 
     
-    const file = await storage.getFileDownload(process.env.APPWRITE_BUCKET_ID, fileID)
-
-    var enc = new TextDecoder("utf-8");
-    const csv = enc.decode(file)
+    // const file = await storage.getFileDownload(env.APPWRITE_BUCKET_ID, fileID)
     
-    return {
-        "fileContent": csv
-    };
+        // var enc = new TextDecoder("utf-8");
+        // const csv = enc.decode(file)
+    const paths = getRouterParam(event, 'index')
+    console.log(paths);
+    
+    if (paths == "centroid_clipped" || paths == "burned_area_clipped") {
+        const data = await useStorage('assets:server').getItem(`centroid_clipped.json`)
+        return data
+    }
+
+    const params = paths.split("_")
+
+    const data = await useStorage('assets:server').getItem(`${params[0]}/${params[1]}/${params[2]}.csv`)
+
+    return {data: data}
 });
